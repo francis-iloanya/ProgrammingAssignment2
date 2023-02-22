e83c548cd7e9285ee55789d390fb1b1da31a9d0f
Francis Iloanya <francis.iloanya@outlook.com>
2023-02-22 22:47:20 +0000

    Francis' Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Programming Assignment 2: Lexical Scoping 
 
-## Write a short comment describing this function
+## The first line sets up the function with the x argument being defaulted to a matrix
+## Then create an empty object called inverse where the inverse will be stored
+## Set function will assign new matrix to X and reset inverse value to NULL ready to be populated
+## Get function returns the matrix
+## Set inverse when called assigns value of inverse matrix into parent environment
+## Get inverse when called retrieves value of inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x=matrix()){       
+        inverse <- NULL                         
+        set <- function(y) {                 
+                x <<- y
+                inverse <<- NULL                
+        }
+        
+        get <- function() x                     
+        
+        
+        setinverse <- function(inv) inverse <<- inv  
+        getinverse <- function() inverse                
+        
+        list(set = set, get = get,  
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Searches for inverse first - if found, returns value, otherwise continues through rest of the function which...
+## ...gets the matrix, solves the inverse, stores it, the prints it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("retreiving cached data")
+                return(inverse)
+        }                                       
+        matrix <- x$get()
+        inverse <- solve(matrix, ...)
+        x$setinverse(inverse)
+        inverse
 }
